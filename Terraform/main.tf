d7c4e23a2c49b683217018d6ec3bb458c5324caf
DarbhaPreetham <preetham.darbha@gmail.com>
2024-02-29 17:54:34 +0530

    deploy-infra-maintf-8

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -20,6 +20,7 @@ resource "aws_instance" "server" {
     key_name = aws_key_pair.deployment.key_name 
     vpc_security_group_ids = [aws_security_group.maingroup.id]
     iam_instance_profile = aws_iam_instance_profile.ec2-profile.name
+    subnet_id = "subnet-057e537849cbd8c38"
     
     connection {
       type = "ssh"
